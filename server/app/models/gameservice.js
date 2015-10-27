fe4dcce4f55f1810511ed630f76ac27089e3c242
pacchy <SPRao@BLRW3003475.internal.imsglobal.com>
2015-10-27 17:07:21 +0530

    new game
    save new game to db

--- server/app/models/gameservice.js
+++ server/app/models/gameservice.js
@@ -3,6 +3,9 @@
 	
 	var mongoose = require('mongoose');	 
 	var db = mongoose.connect('mongodb://127.0.0.1:27017/chess');
+	var Schema = mongoose.Schema;
+	var gameSchema = new Schema({});
+	var Game = mongoose.model('game', gameSchema);
 
 	var getGameById = function(id, callbackFn){
 	  var game = {id:12345, player1:{name:'prashanth', playingWhite:true}, player2:{name:'test', playingWhite:false}};
@@ -16,9 +19,18 @@
 	   callbackFn(games);
 	};
 
-	var createGame = function(game, callbackFn){
-		
+	var createGame = function(gameParam, callbackFn){
+		var gameid = getNewGameId();
+		var game = new Game({id:gameId, player1:{name:'prashanth', playingWhite:true}, player2:{name:'test', playingWhite:false}});
+		game.save();
 	    callbackFn(game);
+	};
+
+	var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	var getNewGameId = function(){
+		var gameId = '';
+		for (var i = 5; i > 0; --i) gameId += chars[Math.round(Math.random() * (chars.length - 1))];    
+		return gameId;
 	};
 
 	var move = function(move, callbackFn){
